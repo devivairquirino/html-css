276db62b1243c09471e516d6ca13392ea7cc7255
Ivair Quirino <123018648+devivairquirino@users.noreply.github.com>
2023-02-09 17:05:20 -0300

    mq prof

--- curso-video/Exercicios/ex026/mq005prof/menu.js
+++ curso-video/Exercicios/ex026/mq005prof/menu.js
@@ -1,7 +1,19 @@
+
 function clickMenu(){
     const menu= document.getElementById('imenu');
+
     
     if(menu.style.display==='none'){
+        menu.style.display='block';
+    }
+    else{
+        menu.style.display='none';
+    }
+}
+
+function mudouTamanho(){
+    const menu= document.getElementById('imenu');
+    if(window.innerWidth>= 768){
         menu.style.display='block';
     }
     else{
