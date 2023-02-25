2a6168bc57eae99d14d11142422f0ed8c68c99a8
Ivair Quirino <123018648+devivairquirino@users.noreply.github.com>
2023-02-25 15:56:46 -0300

    atulizando botao

--- praticando-flex-grid/temlate003/mudarImagem.js
+++ praticando-flex-grid/temlate003/mudarImagem.js
@@ -1,13 +1,27 @@
-function alterarImg(botao){
-    let tela=document.getElementById('card-video');
-    let texto_tela=document.getElementsByTagName('p');
-    let img_tela=document.getElementsByTagName('img');
+function alterarImg(n){
+    let banner = document.getElementsByTagName('img')[1];
+    let img_pq= document.getElementsByTagName('img')[n+1];
+    // Variavel de apoio
+    let temp_img= img_pq.src;
+
+    // Alterando o endereço das imgs
+    img_pq.src=banner.src;
+    banner.src=temp_img;
+
+    let txt_banner = document.getElementsByTagName('p')[0];
+    let txt_pq = document.getElementsByTagName('p')[n];
+    // Variavel de apoio
+    let temp_txt= txt_pq.innerHTML;
+
+     // Alterando os textos
+    txt_pq.innerHTML= txt_banner.innerHTML;
+    txt_banner.innerHTML= temp_txt;
+
+
+
+    // let texto_tela=document.getElementsByTagName('p');
+    // let img_tela=document.getElementsByTagName('img');
     
-    let novo_texto=texto_tela[botao].innerHTML;
-    let nova_img= img_tela[botao+1].src;
-   
-    texto_tela[0].innerHTML= novo_texto;
-    
-    img_tela.src=nova_img;
-    
+
+
 }
